a70ae8ed4d89a37201ab234f8babb01b8d848582
Automation Expert <70540068+automationxpert@users.noreply.github.com>
2024-12-31 22:47:44 +0530

    add public folder

--- functions/webhook.js
+++ functions/webhook.js
@@ -4,7 +4,7 @@ exports.handler = async (event, context) => {
     const payload = JSON.parse(event.body);
 
     try {
-        const response = await axios.post('https://another-webhook-url.com', payload);
+        const response = await axios.post('https://discord.com/api/webhooks/1323687563248009226/sZDN9PChdbnvo2UFmxFYue1TRgw47nGypoz1e33vAOurkL9pVmll5twLm5Xa1OJnvenJ', payload);
         return {
             statusCode: 200,
             body: JSON.stringify({ success: true, data: response.data }),
